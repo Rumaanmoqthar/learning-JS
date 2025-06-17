a478497b8af76bb049b332b22875fc641d35eb50
Rumaanmoqthar <113524045+Rumaanmoqthar@users.noreply.github.com>
2025-06-17 09:13:21 +0000

    again container con fig done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 12344
+let accountEmail = "rumaanmoq@gmail.com"
+accountName = "rumaan moqthar"
+accountAddress = "mysore"
+
+console.table([accountId,accountEmail,accountName,accountAddress])
