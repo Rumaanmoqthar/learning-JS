30d07051292e23c843d48f17948ca6eb1b48b735
Rumaanmoqthar <113524045+Rumaanmoqthar@users.noreply.github.com>
2025-06-19 11:36:38 +0000

    #common datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//alter (3+3) works on browser not in node.js
+
+console.log(3+3);
+
+let name = "rumaan" //string
+let age = 13 // number
+let isLoggedIn = false // boolean
+//bigint
+//null is a standalone value it is recongnise as object also null is a 
+//representation of empty 
+//symbol represents uniqueness
