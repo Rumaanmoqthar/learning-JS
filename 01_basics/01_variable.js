d18133cc1c95f4f98a6a73ef2a9a652fab5b5f4c
Rumaanmoqthar <113524045+Rumaanmoqthar@users.noreply.github.com>
2025-06-19 11:27:02 +0000

    #WIP

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -4,3 +4,6 @@ accountName = "rumaan moqthar"
 accountAddress = "mysore"
 
 console.table([accountId,accountEmail,accountName,accountAddress])
+
+// accountID = 3 not possible because of const
+// prefer not to use var because of the issue of in {} block scope an dfunctional scope
