e96930d4c088a89612e157faaba253374332fd44
Rumaanmoqthar <113524045+Rumaanmoqthar@users.noreply.github.com>
2025-06-19 13:12:35 +0000

    simple data type conversions

---
+++ 01_basics/01_coversion.js
@@ -0,0 +1,22 @@
+//let score = "33abc"
+
+//console.log(typeof score);
+
+//let valueInNmuber = Number (score) // converting from string to number
+//onsole.log(typeof valueInNmuber);
+//onsole.log(valueInNmuber); //NaN (not a number)
+
+//"33" => 33
+//"33abcd" => NaN
+//true =>1 / False => 0
+
+let isLoggedIn = "rumaan "
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//"" => False
+//"rumaan" => true
+
+
+
+
