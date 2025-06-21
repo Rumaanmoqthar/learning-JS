e264e44bc3b4423d07497695271c939c7d5ba02c
Rumaanmoqthar <113524045+Rumaanmoqthar@users.noreply.github.com>
2025-06-21 07:31:44 +0000

    summary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+//console.log(2 > 2);
+//console.log(2 >= 2);
+//console.log(2 <= 2);
+//console.log(2 == 2);
+//console.log(2 =! 2);
+
+console.log(null > 0);//false
+console.log(null == 0); //false
+console.log(null >= 0); // true because it converts null into zero 
+
+
+console.log(null > 0);
+console.log(null == 0); 
+console.log(null >= 0);
+
+console.log("2" === 2); // it stricly search the both data type and value 
+
---
+++ 01_basics/datatypessummary.js
@@ -0,0 +1,27 @@
+//two types of data that is sttored in memory and access
+//#primitive
+
+//7 types :-null, number, boolean, string, undifined,symbol, bigint 
+
+const score = 100 // number
+const scorecard = 100.3 //number
+const isLoggedIn = false //boolean
+const outsideeTemp = null
+let useremail; //undefined
+
+const id = Symbol('123')
+
+//reference data type (non primitive)
+//array,object,function
+
+const hero = ["iron_man","thor","hulk"] //array
+
+let myobj = {
+    name:"rumaan", //objects
+    age:"23"
+}
+
+ const myFunction = function(){
+    console.log("hello world");  
+    
+ }
